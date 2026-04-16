ade3ec803efa7e273ca4869ecef4eca439900feb
uyauyapoh <fashadimas04@gmail.com>
2026-04-17 00:39:00 +0700

    fix frontend serve properly

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+// 🔥 WAJIB DI ATAS
+app.use(express.static(path.join(__dirname, 'public')));
+
+// routes API
 app.use('/booking', require('./routes/booking'));
 app.use('/upload', require('./routes/upload'));
 app.use('/user', require('./routes/user'));
 
-app.get('/', (req, res) => {
-  res.send('Sehat Antri API Running');
-});
+// app.get('/', (req, res) => {
+//   res.send('Sehat Antri API Running');
+// });
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log('Server running on port', PORT);
 });
-
-const path = require('path');
-
-// serve frontend
-app.use(express.static(path.join(__dirname, 'public')));
